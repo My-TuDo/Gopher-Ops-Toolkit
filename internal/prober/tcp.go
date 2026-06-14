938ad4d66ac692a2b6d2bf861f09163cf628fb4e
My-TuDo <2647601953@qq.com>
2026-06-14 14:39:27 +0800

    重构tcp结构

---
+++ internal/prober/tcp.go
@@ -0,0 +1,41 @@
+package prober
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+// 实现 TCP 端口探测的结构体
+type TCPResult struct{}
+
+func (t TCPResult) checkTCP(host string, port int, timeout time.Duration) Result {
+	// 记录开始时间
+	start := time.Now()
+
+	// 尝试连接 TCP 端口
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), timeout)
+
+	// 计算响应时间
+	latency := time.Since(start).Milliseconds()
+
+	// 连接失败
+	if err != nil {
+		return Result{
+			Name:    "TCP探测",
+			Target:  fmt.Sprintf("%s:%d", host, port),
+			Status:  "不健康",
+			Error:   err.Error(),
+			Latency: latency,
+		}
+	}
+	defer conn.Close()
+
+	// 连接成功
+	return Result{
+		Name:    "TCP探测",
+		Target:  fmt.Sprintf("%s:%d", host, port),
+		Status:  "健康",
+		Latency: latency,
+	}
+}

dd0a3a25344606aff418dac207b01eaa37a6c708
My-TuDo <2647601953@qq.com>
2026-06-16 16:02:00 +0800

    feat:构筑dns探测方法。

--- internal/prober/dns.go
+++ internal/prober/dns.go
@@ -1,3 +1,16 @@
 package prober
 
+import "time"
+
 type DNSProber struct{}
+
+func (d DNSProber) Probe(target string, timeout time.Duration) Result {
+
+	return Result{
+		Name:    "DNS探测",
+		Target:  target,
+		Status:  "不健康",
+		Error:   "DNS探测功能尚未实现",
+		Latency: 0,
+	}
+}
